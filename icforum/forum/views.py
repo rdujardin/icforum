bba7cdd04ad534a302a8d7925145d8845d89943a
Raphaël Dujardin <r.dujardin.7@gmail.com>
2016-08-15 19:11:38 +0200

    Allow youtube videos in messages

--- icforum/forum/views.py
+++ icforum/forum/views.py
@@ -10,13 +10,24 @@ from .forms import *
 
 import random
 import bleach
+from urllib.parse import urlparse
 
 
 def sanitize_html(content):
+	def filter_iframe_src(name, value):
+		if name in ('width', 'height', 'frameborder', 'allowfullscreen'):
+			return True
+		elif name == 'src':
+			p = urlparse(value)
+			return (not p.netloc) or p.netloc == 'youtube.com' or p.netloc == 'www.youtube.com'
+		else:
+			return False
+
 	return bleach.clean(content,
-		tags=['a', 'abbr', 'acronym', 'address', 'area', 'b', 'bdo', 'big', 'blockquote', 'br', 'button', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'dd', 'del', 'dfn', 'dir', 'div', 'dl', 'dt', 'em', 'fieldset', 'font', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'map', 'menu', 'ol', 'optgroup', 'option', 'p', 'pre', 'q', 's', 'samp', 'select', 'small', 'span', 'strike', 'strong', 'sub', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'u', 'tr', 'tt', 'u', 'ul', 'var'],
+		tags=['iframe', 'a', 'abbr', 'acronym', 'address', 'area', 'b', 'bdo', 'big', 'blockquote', 'br', 'button', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'dd', 'del', 'dfn', 'dir', 'div', 'dl', 'dt', 'em', 'fieldset', 'font', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'map', 'menu', 'ol', 'optgroup', 'option', 'p', 'pre', 'q', 's', 'samp', 'select', 'small', 'span', 'strike', 'strong', 'sub', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'u', 'tr', 'tt', 'u', 'ul', 'var'],
 		attributes={
 			'*': ['class', 'id', 'style'],
+			'iframe': filter_iframe_src,
 			'a': ['href'],
 			'font': ['color', 'face', 'size'],
 			'img': ['align', 'alt', 'height', 'src', 'title', 'width'],
