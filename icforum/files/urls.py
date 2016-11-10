2f4cfec7247bc4f0ca00e6571bdc4a29d9c7b2c1
Raphaël D <raphael.dujardin@ensea.fr>
2016-11-10 16:46:30 +0100

    Introduce files reading

--- icforum/files/urls.py
+++ icforum/files/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	
+
+	url(r'^list/$', views.list_files, name='list_files'),
+	url(r'^list/(?P<page>\d+)/$', views.list_files, name='list_files'),
+
+	url(r'^file/(?P<pk>\d+)/$', views.file, name='file'),
+
+	url(r'^chapter/(?P<pk>\d+)/$', views.chapter, name='chapter'),
+
 ]
--- icforum/files/views.py
+++ icforum/files/views.py
@@ -7,6 +7,7 @@ import datetime
 
 from .models import *
 from users.models import *
+from forum.views import home
 from .forms import *
 
 import random
@@ -16,7 +17,6 @@ from urllib.parse import urlparse
 from users.common import sanitize_html, _render
 
 def list_files(request, page=1):
-	tag = get_object_or_404(Tag.objects.all(), pk=pk)
 
 	files = File.objects.all()
 	not_allowed = []
@@ -46,11 +46,52 @@ def list_files(request, page=1):
 		except:
 			display_files = []
 
-	return _render(request, 'forum/list_files.html', {
+	# List tags for each file
+	for file in display_files:
+		file.list_tags = file.tags.all()
+
+	return _render(request, 'files/list_files.html', {
 		'files': display_files,
 		'page': page,
 		'num_pages': range(1, num_pages + 1),
 	})
 
 def file(request, pk):
-	pass
+	file = get_object_or_404(File.objects.all(), pk=pk)
+
+	chapters = Chapter.objects.filter(file__id__exact=file.id).order_by('position')
+
+	for tag in file.tags.all():
+		if tag.only_for.all():
+			allowed = False
+			for allowed_group in tag.only_for.all():
+				if allowed_group in request.user.groups.all():
+					allowed = True
+			if not allowed:
+				return redirect(home)
+
+	return _render(request, 'files/file.html', {
+		'file': file,
+		'chapters': chapters,
+	})
+
+def chapter(request, pk):
+	chapter = get_object_or_404(Chapter.objects.all(), pk=pk)
+
+	# Get tags
+	tags = chapter.file.tags.all()
+
+	# Check that topic does not belong to a tag only for a group the signed in user does not belong to
+	for tag in tags:
+		if tag.only_for.all():
+			allowed = False
+			for allowed_group in tag.only_for.all():
+				if allowed_group in request.user.groups.all():
+					allowed = True
+			if not allowed:
+				return redirect(home)
+
+	# Render
+	return _render(request, 'files/chapter.html', {
+		'chapter': chapter,
+	})
