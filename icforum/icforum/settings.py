1bdc1d7b3b120038e12853443417d165e06a3077
Raphaël D <r.dujardin.7@gmail.com>
2016-11-13 01:13:26 +0100

    Fix static root setting

--- icforum/icforum/settings.py
+++ icforum/icforum/settings.py
@@ -146,9 +146,8 @@ LOCALE_PATHS = (
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Media files (avatars..)
 
